6a5421ba30bac1d3b98b206eb35a506970cdd2d8
jonatas625421 <jonatas.fonseca26@hotmail.com>
2021-01-25 23:26:32 -0300

    Criado listagem de horas.

---
+++ lista_horas.php
@@ -0,0 +1,43 @@
+
+<?php
+$conexao = mysqli_connect("localhost", "root", "", "projeto_campo");
+
+if (!$conexao) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+mysqli_set_charset($conexao,'utf8');
+
+$sql = "select * from time_registration order by id desc";
+$data = mysqli_query($conexao,$sql) or die("Erro");
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<table>
+		<tr>
+			<td>usuario</td>
+			<td>Tempo</td>
+		</tr>
+		<?php
+
+           while ($row = mysqli_fetch_assoc($data)) {
+            
+	       echo "<td>".$row['user_id']."</td>";
+	       echo "<td>".$row['field_time']."</td>";
+         } 
+
+		?>
+	</table>
+
+</body>
+</html>
