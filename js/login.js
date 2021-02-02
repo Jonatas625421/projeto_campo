e73ee4b1e85788fced9ae6a06a1d2ab3c92256f7
Kevin Araújo <kfa_34@hotmail.com>
2021-02-01 22:18:09 -0300

    Organizado código de login.

---
+++ js/login.js
@@ -0,0 +1,13 @@
+function validate_login() { 
+
+    var form = document.getElementById("form");
+    var email = document.getElementsByName("email")[0];
+    var password = document.getElementsByName("password")[0];
+
+    if(email.value == '' || password.value == ''){  
+        alert("Complete os campos");
+        return;  
+    }
+     
+    form.submit();		
+}
