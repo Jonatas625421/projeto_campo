793df04daf880aa7c3e541a4c61c6781950c0198
jonatas625421 <jonatas.fonseca26@hotmail.com>
2021-03-01 22:59:32 -0300

    Adicionado CSS na tela de login

--- login.php
+++ login.php
@@ -14,14 +14,27 @@ if (isset($_SESSION['autenticado'])){
 	<meta charset="utf-8">
 	<title>Tela de login</title>
 	<script src="js/login.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/login.css">
 </head>
-<body>
-<p>Login</p>
-	<form id="form" method="post" action="login_validate.php">
-		<input type="text" name="email" placeholder="Informe seu login"><br><br>
-		<input type="password" name="password" placeholder="Digite a sua senha"><br><br>
-		<input type="button" onclick="validate_login()" value="ENTRAR"><br><br>
-		<small>Não é registrado?</small><a href="#">Crie uma conta</a>
-	</form>
-</body>
+	<body>
+		<form id="form" method="post" action="login_validate.php">
+			<div id="login">
+			    <span>Login</span>
+				
+				
+					<input type="text" name="email" placeholder="Informe seu login">
+				
+				
+					<input type="password" name="password" placeholder="Digite a sua senha">
+				
+				
+					<button onclick="validate_login()">ENTRAR</button> 
+				
+				<div class="links">
+					<a href="#"><small>Esqueci minha senha</small></a>
+					<a href="#"><small>Crie uma conta</small></a>
+				</div>	
+		    </div>
+	    </form>
+	</body>
 </html>
